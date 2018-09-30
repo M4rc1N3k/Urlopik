74477a27669f62e85373184b215bfd9937b59cab
Marcin Zakrzewski <zakrzews@agh.edu.pl>
2018-09-30 13:21:38 +0200

    major changes
    - inserting do-while and exit condition to make the app working in loops
    - adding information about name and author
    - removing some redundant commands that used to check the values
    (pseudo-debugging)

--- Urlopik/src/main/Main.java
+++ Urlopik/src/main/Main.java
@@ -2,44 +2,79 @@ package main;
 
 import java.time.LocalDate;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		
 		int curYear = LocalDate.now().getYear();
 		int nextYear = LocalDate.now().getYear()+1;
 		
-		String offBinary = Reader.dataFromFile("off_clear.txt");
+		String offBinary = Reader.dataFromFile("off_forCancel.txt");
 		int totalOffCount = 90;
 		
+		int mode = -1;
+		Boolean trigger;
+		
 		Map<LocalDate, Integer> curYearMap = Mapper.generateMap(offBinary, curYear);
-		System.out.println(curYearMap.entrySet());
-		System.out.println(curYearMap.size());
-		
-		Printer.offPeriodsDisplay(curYearMap);
-		
 		
 		Counter curCount = new Counter(curYearMap, totalOffCount);
 		int plannedOffCount = curCount.getPlanned();
 		int usedOffCount = curCount.getUsed();
 		int remainingOffCount = curCount.getRemaining();
-		
-		curCount.summary();
-		
-		curYearMap = Modifier.changeOff(1, remainingOffCount, plannedOffCount, curYearMap);
-		
-		
-		curCount = new Counter(curYearMap, totalOffCount);
-		plannedOffCount = curCount.getPlanned();
-		usedOffCount = curCount.getUsed();
-		remainingOffCount = curCount.getRemaining();
-		
-		curCount.summary();
 
+		Scanner sc = new Scanner(System.in);
 
+		System.out.println("Witaj w programie ===Urlopik===");
+		System.out.println("©2018 by MZ\n");
 		
-		System.out.println();
+		do {
+			trigger = false;
+			try {
+
+				curCount = new Counter(curYearMap, totalOffCount);
+				plannedOffCount = curCount.getPlanned();
+				usedOffCount = curCount.getUsed();
+				remainingOffCount = curCount.getRemaining();
+
+				System.out.println("\nTwoje obecnie zaplanowane okresy urlopu to:");
+				Printer.offPeriodsDisplay(curYearMap);
+				
+				System.out.println();
+				curCount.summary();
+				
+				
+				System.out.println("\nCo chciałbyś zrobić?");
+				System.out.println("\t1) dopisać urlop");
+				System.out.println("\t2) anulować urlop");
+				System.out.println("\t3) zakończyć program");
+
+				mode = Integer.parseInt(sc.next());
+				
+				if (mode == 1 || mode == 2)
+					{curYearMap = Modifier.changeOff(mode, remainingOffCount, plannedOffCount, curYearMap);
+					//TODO: zapis mapy do pliku
+					}
+				else
+					{System.out.println("Do widzenia!");
+					break;}
+				
+				System.out.println();
+
+				
+			} catch (Exception e) {
+//				e.printStackTrace();
+				trigger = true;
+//				sc.next();
+			} 
+			
+		
+
+		} while (mode != 3 || trigger == true);
+		
+		sc.close();
 		
 	}
 
