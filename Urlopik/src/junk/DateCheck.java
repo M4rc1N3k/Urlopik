69884dc185053e65c66b30b6765bf644a342dfcc
Marcin Zakrzewski <zakrzews@agh.edu.pl>
2018-10-19 09:42:37 +0200

    prep for testing

---
+++ Urlopik/src/junk/DateCheck.java
@@ -0,0 +1,26 @@
+package junk;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class DateCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LocalDate a = LocalDate.now();
+		System.out.println(a);
+		
+		LocalDate b = a.plusDays(13);
+		System.out.println(b);
+		
+		DayOfWeek c = b.getDayOfWeek();
+		c.toString();
+		System.out.println(c);
+		
+		System.out.println(a.compareTo(b));
+		System.out.println(b.compareTo(a));
+		
+		
+	}
+
+}
--- Urlopik/src/junk/Enums.java
+++ Urlopik/src/junk/Enums.java
@@ -1,4 +1,4 @@
-package test;
+package junk;
 
 public class Enums {
 
