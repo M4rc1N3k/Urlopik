225f96fa6776f9ac101cbc1f1f207e9472dc9d38
Marcin Zakrzewski <markonzak@gmail.com>
2019-05-23 22:01:06 +0200

    SerializerTest stub

---
+++ Urlopik/src/test/mz/SerializerTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SerializerTest {
+
+    @Test
+    public void shouldReturnProperlyFormattedJSON()
+    {
+        Assert.assertEquals(Boolean.TRUE, Boolean.TRUE);
+    }
+}
